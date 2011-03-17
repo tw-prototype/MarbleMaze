06012e5e66b2522c0e1aab78040b38259de2e005
unknown <sandeeb@.corporate.thoughtworks.com>
2011-03-17 21:37:10 +0530

    added code for destination hole

--- src/com/thoughtworks/mm/PhysicsCollisionFilteringExample.java
+++ src/com/thoughtworks/mm/PhysicsCollisionFilteringExample.java
@@ -4,12 +4,14 @@ import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.particle.Particle;
 import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.shape.Shape;
 import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
 import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
 import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
@@ -18,6 +20,7 @@ import org.anddev.andengine.extension.physics.box2d.util.Vector2Pool;
 import org.anddev.andengine.input.touch.TouchEvent;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.sensor.accelerometer.AccelerometerData;
@@ -30,6 +33,7 @@ import android.widget.Toast;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 /**
@@ -48,15 +52,18 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 	public static final short CATEGORYBIT_WALL = 1;
 	public static final short CATEGORYBIT_BOX = 2;
 	public static final short CATEGORYBIT_CIRCLE = 4;
+	public static final short CATEGORYBIT_HOLE = 8;
 
 	/* And what should collide with what. */
 	public static final short MASKBITS_WALL = CATEGORYBIT_WALL + CATEGORYBIT_BOX + CATEGORYBIT_CIRCLE;
 	public static final short MASKBITS_BOX = CATEGORYBIT_WALL + CATEGORYBIT_BOX; // Missing: CATEGORYBIT_CIRCLE
 	public static final short MASKBITS_CIRCLE = CATEGORYBIT_WALL + CATEGORYBIT_CIRCLE; // Missing: CATEGORYBIT_BOX
+	public static final short MASKBITS_HOLE = 0; // Missing: everything
 
 	public static final FixtureDef WALL_FIXTURE_DEF = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f, false, CATEGORYBIT_WALL, MASKBITS_WALL, (short)0);
 	public static final FixtureDef BOX_FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f, false, CATEGORYBIT_BOX, MASKBITS_BOX, (short)0);
 	public static final FixtureDef CIRCLE_FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f, false, CATEGORYBIT_CIRCLE, MASKBITS_CIRCLE, (short)0);
+	public static final FixtureDef HOLE_FIXTURE_DEF = PhysicsFactory.createFixtureDef(0, 0.0f, Float.POSITIVE_INFINITY, false, CATEGORYBIT_HOLE, MASKBITS_HOLE, (short)0);
 
 	// ===========================================================
 	// Fields
@@ -70,6 +77,7 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 	private PhysicsWorld mPhysicsWorld;
 
 	private int mFaceCount = 0;
+	private TiledTextureRegion mHoleTextureRegion;
 
 	// ===========================================================
 	// Constructors
@@ -85,9 +93,9 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 
 	@Override
 	public Engine onLoadEngine() {
-		Toast.makeText(this, "Touch the screen to add objects.", Toast.LENGTH_LONG).show();
+	/*	Toast.makeText(this, "Touch the screen to add objects.", Toast.LENGTH_LONG).show();
 		Toast.makeText(this, "Boxes will only collide with boxes.\nCircles will only collide with circles.", Toast.LENGTH_LONG).show();
-		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+	*/	final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
 		engineOptions.getTouchOptions().setRunOnUpdateThread(true);
 		return new Engine(engineOptions);
@@ -98,12 +106,13 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 		/* Textures. */
 		this.mTexture = new Texture(64, 64, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 		TextureRegionFactory.setAssetBasePath("gfx/");
-
+		Texture mTexture1 = new Texture(128, 128, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 		/* TextureRegions. */
-		this.mBoxFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_box_tiled.png", 0, 0, 2, 1); // 64x32
-		this.mCircleFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_circle_tiled.png", 0, 32, 2, 1); // 64x32
+	/*	this.mBoxFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_box_tiled.png", 0, 0, 2, 1); // 64x32
+	*/	this.mCircleFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_circle_tiled.png", 0, 32, 2, 1); // 64x32
+		this.mHoleTextureRegion = TextureRegionFactory.createTiledFromAsset(mTexture1, this, "hole.png",0,0,2,1); // 64x32
 		this.mEngine.getTextureManager().loadTexture(this.mTexture);
-
+		this.mEngine.getTextureManager().loadTexture(mTexture1);
 		this.enableAccelerometerSensor(this);
 	}
 
@@ -115,23 +124,27 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 		scene.setBackground(new ColorBackground(0, 0, 0));
 		scene.setOnSceneTouchListener(this);
 
-		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_DEATH_STAR_I), false);
 
 		final Shape ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2);
 		final Shape roof = new Rectangle(0, 0, CAMERA_WIDTH, 2);
 		final Shape left = new Rectangle(0, 0, 2, CAMERA_HEIGHT);
 		final Shape right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT);
-
+		final Shape center = new Rectangle(CAMERA_WIDTH/2,CAMERA_HEIGHT/2 , 50, CAMERA_WIDTH/2);
+		final Shape hole = new AnimatedSprite(2,2, this.mHoleTextureRegion);
+		
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, WALL_FIXTURE_DEF);
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, WALL_FIXTURE_DEF);
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, left, BodyType.StaticBody, WALL_FIXTURE_DEF);
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, right, BodyType.StaticBody, WALL_FIXTURE_DEF);
-
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, center, BodyType.StaticBody, WALL_FIXTURE_DEF);
+		PhysicsFactory.createCircleBody(this.mPhysicsWorld, hole, BodyType.StaticBody, HOLE_FIXTURE_DEF);
 		scene.getFirstChild().attachChild(ground);
 		scene.getFirstChild().attachChild(roof);
 		scene.getFirstChild().attachChild(left);
 		scene.getFirstChild().attachChild(right);
-
+		scene.getFirstChild().attachChild(center);
+		scene.getFirstChild().attachChild(hole);
 		scene.registerUpdateHandler(this.mPhysicsWorld);
 
 		return scene;
@@ -139,17 +152,17 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 
 	@Override
 	public void onLoadComplete() {
-
+		this.addFace(360,240);
 	}
 
 	@Override
 	public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
-		if(this.mPhysicsWorld != null) {
+		/*if(this.mPhysicsWorld != null) {
 			if(pSceneTouchEvent.isActionDown()) {
 				this.addFace(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
 				return true;
 			}
-		}
+		}*/
 		return false;
 	}
 
@@ -167,7 +180,7 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 	private void addFace(final float pX, final float pY) {
 		final Scene scene = this.mEngine.getScene();
 
-		this.mFaceCount++;
+	/*	this.mFaceCount++;
 		Debug.d("Faces: " + this.mFaceCount);
 
 		final AnimatedSprite face;
@@ -180,7 +193,12 @@ public class PhysicsCollisionFilteringExample extends BaseExample implements IAc
 			face = new AnimatedSprite(pX, pY, this.mCircleFaceTextureRegion);
 			body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, CIRCLE_FIXTURE_DEF);
 		}
+	 */
+		final AnimatedSprite face;
+		final Body body;
 
+		face = new AnimatedSprite(pX, pY, this.mCircleFaceTextureRegion);
+		body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, CIRCLE_FIXTURE_DEF);
 		face.animate(200);
 
 		scene.getLastChild().attachChild(face);
