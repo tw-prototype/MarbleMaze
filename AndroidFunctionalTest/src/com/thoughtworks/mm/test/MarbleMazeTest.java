93f040de69fb883cdf921b89d96fa863d7f415bf
Rahul Joshi <joshir3@gsicommerce.com>
2011-03-29 14:19:35 +0530

    test case updated

--- AndroidFunctionalTest/src/com/thoughtworks/mm/test/MarbleMazeTest.java
+++ AndroidFunctionalTest/src/com/thoughtworks/mm/test/MarbleMazeTest.java
@@ -1,24 +1,33 @@
 package com.thoughtworks.mm.test;
 
 import com.jayway.android.robotium.solo.Solo;
-import com.thoughtworks.mm.MarbleMaze;
 
 import android.test.ActivityInstrumentationTestCase2;
+import com.thoughtworks.mm.MarbleMazeActivity;
 
 public class MarbleMazeTest extends
-		ActivityInstrumentationTestCase2<MarbleMaze> {
+		ActivityInstrumentationTestCase2<MarbleMazeActivity> {
 
 	private Solo solo;
 
-	public MarbleMazeTest(Class<MarbleMaze> activityClass) {
+	public MarbleMazeTest(Class<MarbleMazeActivity> activityClass) {
 		super("com.thoughtworks.mm", activityClass);
 	}
+
+    public MarbleMazeTest() {
+        super("com.thoughtworks.mm", MarbleMazeActivity.class);
+    }
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
+
+
+    public void testSample() {
+        assertTrue(true);
+    }
 
 	@Override
 	protected void tearDown() throws Exception {
